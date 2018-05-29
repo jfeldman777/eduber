f1424d77bbf63143019d88bb0efcf902ad55e033
jfeldman777 <jfeldman777@gmail.com>
2018-05-29 20:11:42 +0300

    map

--- eduber/urls.py
+++ eduber/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('land.urls')),    
+    path('land/', include('land.urls')),     
+    path('', include('land.urls')),
 ]
--- land/urls.py
+++ land/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('map/', views.map, name='map'),
     path('', views.index, name='index'),
 ]
