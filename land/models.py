2c58ba4fb9d2777fff6c43a8051ce4f4bd03e342
jfeldman777 <jfeldman777@gmail.com>
2018-06-01 16:59:04 +0300

    db lat lng

--- land/models.py
+++ land/models.py
@@ -8,6 +8,9 @@ class Parent(models.Model):
     address = models.CharField(max_length = 50, blank=True, null = True)
     phone = models.CharField(max_length = 20, blank = False)
 
+    lat = models.FloatField(default=59.93863)
+    lng = models.FloatField(default=30.31413)
+
     def __str__(self):
         return self.first_name + " " + self.otch + " " + self.last_name
 
@@ -17,4 +20,4 @@ class Kid(models.Model):
     date_of_birth = models.DateField(blank=True, null=True)
     face = models.ImageField(upload_to='uploads/%Y/%m/%d', blank=True, null=True)
     def __str__(self):
-        return self.name + "(" + str(self.parent) + ")"
+        return self.name + " " + self.parent.last_name + " "
