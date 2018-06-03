8b15cd40bc10c61bd816350b2226f697b17eda76
jfeldman777 <jfeldman777@gmail.com>
2018-06-03 15:08:03 +0300

    clean

--- land/views.py
+++ land/views.py
@@ -21,12 +21,6 @@ def xin(request):
 def about(request):
     return render(request,'about.html')
 
-#def map(request):
-#    return render(request,'map.html')
-
-#def gps(request):
-#    return render(request,'gps.html')
-
 def demo1(request):
     return render(request,'demo1.html')
 
@@ -35,14 +29,3 @@ def demo2(request):
 
 def demo3(request):
     return render(request,'demo3.html')
-
-#def demo(request,lat,lng):
-#    request.session["lat"] = lat;
-#    request.session["lng"] = lng;
-#    return render(request,'demo.html')
-
-#def demo_map(request):
-    #d = {}
-    #d['lat'] = request.session.get('lat',0)
-    #d['lng'] = request.session.get('lng',0)
-    #return render(request,'demo_map.html',d)
