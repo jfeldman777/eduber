32bade29ec7213e3e1bb6b64bd90415ada17a862
jfeldman777 <jfeldman777@gmail.com>
2018-06-03 15:33:06 +0300

    drop

--- land/admin.py
+++ land/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-
-from .models import Parent, Kid
-# Register your models here.
-
-class ParentAdmin(admin.ModelAdmin):
-    pass
-
-class KidAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Parent, ParentAdmin)
-admin.site.register(Kid, KidAdmin)
