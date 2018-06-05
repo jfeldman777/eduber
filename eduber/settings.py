fa737a647c25028c79a9fd25542e2137a06ae257
jfeldman777 <jfeldman777@gmail.com>
2018-06-05 12:08:22 +0300

    demo template

--- eduber/settings.py
+++ eduber/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'eduber.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': ['templates','templates/demo'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
