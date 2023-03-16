10a7a8894846a3aeeacec4f1e54cf887ce4b19df
jfeldman777 <jfeldman777@gmail.com>
2023-03-16 11:09:45 +0200

    new era

--- land/models.py
+++ land/models.py
@@ -3,6 +3,19 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from django.db import models
+
+
+class CustomBigAutoField(models.BigAutoField):
+    """Int8 field that is applied only for new models."""
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        if getattr(self, 'model', None):
+            path = 'django.db.models.AutoField'
+        return name, path, args, kwargs
+
+
 class QPage(models.Model):
     hide = models.BooleanField(default=False)
     code = models.CharField(max_length=20)
